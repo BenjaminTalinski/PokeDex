9ba42e568c796b8d57665c6a1519feb6494226de
Benjamin Talinski <benny13t@gmx.de>
2026-03-28 03:06:13 +0100

    load & show Data names, simple style

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
-const apiBase = "https://pokeapi.co/api/v2/pokemon";
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
 const limit = 26;
 let offset = 0;
 let isLoading = false;
 
-async function loadInitialPokemon() {
-if (isLoading) return;
-isLoading = true; 
+async function loadData() {
+    if (isLoading) return;
+    isLoading = true;
+
+    try {
+        let response = await fetch(BASE_URL + "?limit=" + limit + "&offset=" + offset);
+        let responseAsJson = await response.json();
+        responseAsJson.results.length;
+        console.log(responseAsJson);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        isLoading = false;
+    }
+
 }
+
+
+
+
+
+
+loadData();
