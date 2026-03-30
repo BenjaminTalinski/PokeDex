250238378fdb8c15437b2694aade4e10ee48aa40
Benjamin Talinski <benny13t@gmx.de>
2026-03-30 23:50:54 +0200

    push deleted bug

--- js/script.js
+++ js/script.js
@@ -4,36 +4,33 @@ let offset = 0;
 let isLoading = false;
 
 async function loadData() {
-    if (isLoading) return;
-    isLoading = true;
+  if (isLoading) return;
+  isLoading = true;
 
-    try {
-        let response = await fetch(BASE_URL + "?limit=" + limit + "&offset=" + offset);
-        let responseAsJson = await response.json();
-        showData(responseAsJson.results)
-    } catch (error) {
-        console.log(error);
-    } finally {
-        isLoading = false;
-    }
-
+  try {
+    let response = await fetch(
+      BASE_URL + "?limit=" + limit + "&offset=" + offset,
+    );
+    let responseAsJson = await response.json();
+    showData(responseAsJson.results);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    isLoading = false;
+  }
 }
 
 function showData(pokemonList) {
-    let pkmBox = document.getElementById("pkmList");
-    for (let i = 0; i < pokemonList.length; i++) {
-        let pkmList = document.createElement("div")
-        pkmList.classList.add("pkmCard");
-        let pkmNames = document.createElement("p")
-        pkmNames.classList.add("pkmName");
-        pkmNames.innerText = pokemonList[i].name;
-        pkmList.appendChild(pkmNames);
-        pkmBox.appendChild(pkmList);
-
-    }
-
+  let pkmBox = document.getElementById("pkmList");
+  for (let i = 0; i < pokemonList.length; i++) {
+    let pkmList = document.createElement("div");
+    pkmList.classList.add("pkmCard");
+    let pkmNames = document.createElement("p");
+    pkmNames.classList.add("pkmName");
+    pkmNames.innerText = pokemonList[i].name;
+    pkmList.appendChild(pkmNames);
+    pkmBox.appendChild(pkmList);
+  }
 }
-
-
 
 loadData();
