ec55b1db4b67e72af83c4c5b9dbdc99ea92c100f
Benjamin Talinski <benny13t@gmx.de>
2026-04-05 05:09:55 +0200

    new function for create pokemon Card

--- js/script.js
+++ js/script.js
@@ -35,62 +35,65 @@ async function loadDetailData(detailUrl) {
   return responseAsJson;
 }
 
+function createPkmCard(detailData) {
+  let pkmList = document.createElement("div");
+  pkmList.classList.add("pkmCard");
+  let pkmNames = document.createElement("p");
+  pkmNames.classList.add("pkmName");
+  let pkmWeight = document.createElement("p");
+  pkmWeight.classList.add("pkmWeight");
+  let pkmHeight = document.createElement("p");
+  pkmHeight.classList.add("pkmHeight");
+  let pkmImg = document.createElement("img");
+  pkmImg.classList.add("pkmImg");
+  let pkmImgShiny = document.createElement("img");
+  pkmImgShiny.classList.add("pkmImgShiny");
+  let toggleBtn = document.createElement("button");
+  toggleBtn.classList.add("toggleBtn");
+  let baseName =
+    detailData.name.charAt(0).toUpperCase() + detailData.name.slice(1);
+
+  pkmImg.src = detailData.sprites.front_default;
+  pkmImgShiny.src = detailData.sprites.front_shiny;
+
+  pkmNames.innerText =
+    detailData.name.charAt(0).toUpperCase() + detailData.name.slice(1);
+  pkmWeight.innerText = "Weight: " + detailData.weight;
+  pkmHeight.innerText = "Height: " + detailData.height;
+  toggleBtn.innerText = "Shiny";
+
+  toggleBtn.addEventListener("click", function () {
+    if (pkmImg.style.display !== "none") {
+      pkmImg.style.display = "none";
+      pkmImgShiny.style.display = "block";
+      toggleBtn.innerText = "Default";
+      pkmNames.innerText = baseName + " (Shiny)";
+    } else {
+      pkmImgShiny.style.display = "none";
+      pkmImg.style.display = "block";
+      toggleBtn.innerText = "Shiny";
+      pkmNames.innerText = baseName;
+    }
+  });
+
+  pkmList.appendChild(pkmNames);
+  pkmList.appendChild(pkmImg);
+  pkmList.appendChild(pkmImgShiny);
+  pkmList.appendChild(pkmWeight);
+  pkmList.appendChild(pkmHeight);
+  pkmList.appendChild(toggleBtn);
+
+  return pkmList;
+}
+
 async function showData(pokemonList) {
   let pkmBox = document.getElementById("pkmList");
 
   for (let i = 0; i < pokemonList.length; i++) {
     let detailData = await loadDetailData(pokemonList[i].url);
-    let pkmList = document.createElement("div");
-    pkmList.classList.add("pkmCard");
-    let pkmNames = document.createElement("p");
-    pkmNames.classList.add("pkmName");
-    let pkmWeight = document.createElement("p");
-    pkmWeight.classList.add("pkmWeight");
-    let pkmHeight = document.createElement("p");
-    pkmHeight.classList.add("pkmHeight");
-    let pkmImg = document.createElement("img");
-    pkmImg.classList.add("pkmImg");
-    let pkmImgShiny = document.createElement("img");
-    pkmImgShiny.classList.add("pkmImgShiny");
-    let toggleBtn = document.createElement("button");
-    toggleBtn.classList.add("toggleBtn");
-    let baseName =
-      detailData.name.charAt(0).toUpperCase() + detailData.name.slice(1);
-    pkmNames.innerText = baseName;
+    let pkmCard = createPkmCard(detailData);
 
-    pkmNames.innerText = pokemonList[i].name;
-    pkmImg.src = detailData.sprites.front_default;
-    pkmImgShiny.src = detailData.sprites.front_shiny;
-    pkmWeight.innerText = detailData.weight;
-    pkmHeight.innerText = detailData.height;
-
-    pkmNames.innerText =
-      detailData.name.charAt(0).toUpperCase() + detailData.name.slice(1);
-    pkmWeight.innerText = "Weight: " + detailData.weight;
-    pkmHeight.innerText = "Height: " + detailData.height;
-    toggleBtn.innerText = "Shiny";
-
-    toggleBtn.addEventListener("click", function () {
-      if (pkmImg.style.display !== "none") {
-        pkmImg.style.display = "none";
-        pkmImgShiny.style.display = "block";
-        toggleBtn.innerText = "Default";
-        pkmNames.innerText = baseName + " (Shiny)";
-      } else {
-        pkmImgShiny.style.display = "none";
-        pkmImg.style.display = "block";
-        toggleBtn.innerText = "Shiny";
-        pkmNames.innerText = baseName;
-      }
-    });
-
-    pkmBox.appendChild(pkmList);
-    pkmList.appendChild(pkmNames);
-    pkmList.appendChild(pkmImg);
-    pkmList.appendChild(pkmImgShiny);
-    pkmList.appendChild(pkmWeight);
-    pkmList.appendChild(pkmHeight);
-    pkmList.appendChild(toggleBtn);
+    pkmBox.appendChild(pkmCard);
   }
 }
 
